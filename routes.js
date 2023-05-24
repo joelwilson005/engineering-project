d190253cf516a8ff3a46b3b293bc2cd5b794d744
joel <joelwilsonjam@gmail.com>
2023-05-24 12:28:16 -0500

    Removed node_modules folder

---
+++ routes.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+
+
+router.get('/police', (req, res) => {
+    res.render('police')
+})
+
+
+router.get('/police-clarendon', (req, res) => {
+    res.render('police-clarendon')
+})
+
+
+
+router.get('/012546', (req, res) => {
+    res.render('012546')
+})
+
+
+
+
+
+// Get data from API and render it on webpage. 
+
+var apiUrl = ""
+
+
+
+module.exports = router;
