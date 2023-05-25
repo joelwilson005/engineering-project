9ac19adc2ea511c92f8ac41531e727ef6cecb513
joel <joelwilsonjam@gmail.com>
2023-05-25 15:50:41 -0500

    fixed multiple bugs

--- public/javascripts/data-fetch.js
+++ public/javascripts/data-fetch.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   let temperatureInterval;
   let heartRateInterval;
   let locationInterval;
+  let data;
+  let currentIndex;
 
   $('#button').click(function() {
     // Save the original button text
@@ -25,29 +27,32 @@ $(document).ready(function() {
         clearInterval(interval);
         $('#progress-bar-container').hide();
         $('#button').text(originalButtonText);
-        fetchData();
-        temperatureInterval = setInterval(updateTemperature, 20000); // Update temperature every 20 seconds
+        if (!data) {
+          fetchData();
+        }
+        temperatureInterval = setInterval(updateTemperature, 7000); // Update temperature every 7 seconds
         heartRateInterval = setInterval(updateHeartRate, 60000); // Update heart rate every 1 minute
-        locationInterval = setInterval(updateLocation, 15000); // Update location every 15 seconds
+        locationInterval = setInterval(updateLocation, 9000); // Update location every 9 seconds
       }
     }, randomTime / 10);
   });
 
-  // Function to fetch data from the server and update the UI
+  // Function to fetch data from the server
   function fetchData() {
     $.ajax({
       url: '/data.json',
       type: 'GET',
       dataType: 'json',
-      success: function(data) {
+      success: function(response) {
+        data = response;
         // Randomly select an object from the array
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const randomObject = data[randomIndex];
+        currentIndex = Math.floor(Math.random() * data.length);
+        const randomObject = data[currentIndex];
 
         // Update the UI with the selected object's data
         $('#stats-container').show();
-        updateTemperature(randomObject.body_temperature);
-        updateHeartRate(randomObject.heart_rate);
+        updateTemperature();
+        updateHeartRate();
         updateLocation(randomObject.latitude, randomObject.longitude);
       },
       error: function(xhr, status, error) {
@@ -57,37 +62,42 @@ $(document).ready(function() {
   }
 
   // Function to update the temperature in the UI
-  function updateTemperature(temperature) {
-    $('#body_temperature').text( temperature + " \u00B0" + "C");
+  function updateTemperature() {
+    if (data && data.length > 0) {
+      const randomObject = data[currentIndex];
+      $('#body_temperature').text(randomObject.body_temperature + "\u00B0" + "C");
 
-    // Update temperature every 20 seconds
-    setTimeout(function() {
-      fetchData();
-    }, 20000);
+      // Update temperature every 7 seconds
+      setTimeout(updateTemperature, 7000);
+    }
   }
 
   // Function to update the heart rate in the UI
-  function updateHeartRate(heartRate) {
-    $('#heart_rate').text(heartRate + " BPM");
+  function updateHeartRate() {
+    if (data && data.length > 0) {
+      const randomObject = data[currentIndex];
+      $('#heart_rate').text(randomObject.heart_rate + " BPM");
 
-    // Update heart rate every 1 minute
-    setTimeout(function() {
-      fetchData();
-    }, 60000);
+      // Update heart rate every 1 minute
+      setTimeout(updateHeartRate, 60000);
+    }
   }
 
   // Function to update the location in the UI
-  function updateLocation(latitude, longitude) {
-    const randomDeviation = (Math.random() * 2 - 1) / 10000; // Random deviation between -0.0002 and 0.0002
-    const newLatitude = latitude + randomDeviation;
-    const newLongitude = longitude + randomDeviation;
+  function updateLocation() {
+    if (data && data.length > 0) {
+      currentIndex = Math.floor(Math.random() * data.length);
+      const randomObject = data[currentIndex];
+      const randomDeviationLat = (Math.random() * 0.00003) - 0.000015; // Random deviation between -0.000015 and 0.000015
+      const randomDeviationLng = (Math.random() * 0.00003) - 0.000015; // Random deviation between -0.000015 and 0.000015
+      const newLatitude = randomObject.latitude + randomDeviationLat;
+      const newLongitude = randomObject.longitude + randomDeviationLng;
 
-    $('#latitude').text(newLatitude.toFixed(6));
-    $('#longitude').text(newLongitude.toFixed(6));
+      $('#latitude').text(newLatitude.toFixed(6));
+      $('#longitude').text(newLongitude.toFixed(6));
 
-    // Update location every 15 seconds
-    setTimeout(function() {
-      updateLocation(newLatitude, newLongitude);
-    }, 15000);
+      // Update location every 9 seconds
+      setTimeout(updateLocation, 9000);
+    }
   }
 });
